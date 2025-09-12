95ea2276a97da6cc3f59818ed00f8a568b6a080b
Xael <67170424+Xael@users.noreply.github.com>
2025-09-11 22:24:44 -0300

    Add files via upload

--- js/ui/dom.js
+++ js/ui/dom.js
@@ -3,7 +3,4 @@
 // This file acts as a redirector to fix incorrect import paths.
 // It ensures any module trying to load 'dom.js' from within the 'ui' directory
 // gets the correct module from the 'core' directory.
-import { elements, initDom } from '../core/dom.js';
-
-// Re-export the new structure.
-export { elements, initDom };
+export * from '../core/dom.js';
