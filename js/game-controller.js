2fb82ca948855c3ea2e464820f289310d77f9a10
Xael <67170424+Xael@users.noreply.github.com>
2025-09-28 00:45:22 -0300

    Update game-controller.js

--- js/game-controller.js
+++ js/game-controller.js
@@ -13,7 +13,7 @@ import { generateBoardPaths } from './game-logic/board.js';
 import { executeAiTurn } from './ai/ai-controller.js';
 import { createSpiralStarryBackground, resetGameEffects } from './ui/animations.js';
 import { t } from './core/i18n.js';
-import { renderTournamentMatchScore, clearTournamentMatchScore } from '../ui/torneio-renderer.js';
+import { renderTournamentMatchScore, clearTournamentMatchScore } from './ui/torneio-renderer.js';
 
 
 /**
